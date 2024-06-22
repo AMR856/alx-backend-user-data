e6672d7371a1f072671d8b89664ddad2a005d14b
ameralnas <amer.live477@gmail.com>
2024-06-23 02:16:04 +0300

    Trying

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+""" Module of auth
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """This is an auth class"""
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Don't know yet"""
+        if path is None:
+            return True
+        path = path.strip('/')
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        for i in range(len(excluded_paths)):
+            excluded_paths[i] = excluded_paths[i].strip('/')
+        if path in excluded_paths:
+            return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """What is going on here"""
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):  # type: ignore
+        """Current user thing"""
+        return None
--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -14,6 +14,18 @@ def status() -> str:
     return jsonify({"status": "OK"})
 
 
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def error_401() -> None:
+    """401 error view page"""
+    abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def error_403() -> None:
+    """403 error view page"""
+    abort(403)
+
+
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
     """ GET /api/v1/stats
